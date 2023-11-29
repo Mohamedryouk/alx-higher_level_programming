0e7fa4c28372e240be0bbc1a67f265d5ad933ee1
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-11-29 05:57:44 -0800

    done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(_str):
+    result = ""
+    low = range(97, 123)
+    for char in _str:
+        if ord(char) in low:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print(result)
