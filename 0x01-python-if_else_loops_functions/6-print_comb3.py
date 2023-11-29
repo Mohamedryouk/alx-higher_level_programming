4614a2eaacdc9954acdc42b4bc908de8c926b845
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-11-29 04:02:28 -0800

    done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for num in range(10):
     for numb in range(num + 1, 10):
-        print("{:d}{:d}".format(num, numb),end=',' if (num, numb) != (8, 9) else "")
+        output = "{:d}{:d}".format(num, numb)
+        print(output,end=", " if (num, numb) != (8, 9) else "\n")
