07fbaa3cdb2cabe7679a064ccd03388db251faad
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-11-30 11:32:29 -0800

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    arguments = sys.argv[1:]
-    numberOfargs = len(arguments)
-
-    if numberOfargs > 0:
-        print(f"{numberOfargs} arguments :")
-        for index, arg in enumerate(arguments, 1):
-            print(f"{index}: {arg}")
+    sys.argv.pop(0)
+    numberOfargs = len(sys.argv)
+    if numberOfargs == 0:
+        print("0 arguments.")
+    elif numberOfargs == 1:
+        print("1 argument:")
+        print("{:d}: {}".format(len(sys.argv), sys.argv[0]))
     else:
-        print(f"{numberOfargs} arguments .")
+        print("{:d} arguments:".format(numberOfargs))
+        index = 1
+        for arguments in sys.argv:
+            print("{:d}: {}".format(index, arguments))
+            index += 1
