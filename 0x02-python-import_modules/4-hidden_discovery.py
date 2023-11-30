48ea32029e7232d4c8f48b7428191bbee6ac4e37
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-11-30 11:46:02 -0800

    done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     arguments = dir(hidden_4)
     for argument in arguments:
-        if argumen[:2] != "__":
+        if argument[:2] != "__":
             print(argument)
