41ca68514deb3da8337784fb167a623b7ade5332
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-11-30 07:58:54 -0800

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv[1:]
+    numberOfargs = len(arguments)
+
+    if numberOfargs > 0:
+        print(f"{numberOfargs} arguments :")
+        for index, arg in enumerate(arguments, 1):
+            print(f"{index}: {arg}")
+    else:
+        print(f"{numberOfargs} arguments .")
