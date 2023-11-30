12f508f89c835e5748a82e90a33b7731711d584d
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-11-30 11:36:54 -0800

    done

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__name__":
+    import hidden_4
+
+    arguments = dir(hidden_4)
+    for argument in arguments:
+        if argumen[:2] != "__":
+            print(argument)
