6764e17590f268ef2803861e6eed884ea594f23b
root <root@DESKTOP-S8OJ033.>
2023-12-06 20:15:29 +0200

    done

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    removeNumber = []
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list = [value for i, value in enumerate(my_list) if i != idx]
+    return new_list
