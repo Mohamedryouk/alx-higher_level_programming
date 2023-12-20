cfccf5e7f00d0ea30577bfdfcd6f2d56392955b4
root <root@DESKTOP-S8OJ033.>
2023-12-20 16:10:45 +0200

    done

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    raise NameError(message)
-try:
-    raise_exception_msg("This is a custom exception message.")
-except NameError as e:
-    print("Caught an exception:", e)
+    raise NameError
