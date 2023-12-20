0ecff3a8e7ef0dc47d358f788cc177795d9f15d8
root <root@DESKTOP-S8OJ033.>
2023-12-20 16:06:40 +0200

    done

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    raise TypeError("This is a custom type exception.")
-try:
-    raise_exception()
-except TypeError as e:
-    print("Caught an exception:", e)
+    raise TypeError
