faa925ec01836c138366a5d144e819dfc4884987
root <root@DESKTOP-S8OJ033.>
2024-01-09 14:17:51 +0200

    done

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-contains lookup function
+Contains the lookup function
 """
 
 
 def lookup(obj):
-    """return a list of attributes and methods"""
+    """returns a list of available attributes and methods of an object"""
     return dir(obj)
