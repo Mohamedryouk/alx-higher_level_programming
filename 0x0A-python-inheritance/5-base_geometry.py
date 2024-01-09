cc45584c07670239e624afdfc1dcae22eeaa8ca4
root <root@DESKTOP-S8OJ033.>
2024-01-09 21:37:51 +0200

    done

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+contains the class base
+"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+contains the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """A class with public attribute area"""
+    def area(self):
+        """raises an exception flag when called"""
+        raise Exception("arew() is not implemented")
+  
