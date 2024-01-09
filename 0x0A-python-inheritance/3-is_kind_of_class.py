e9fd4961a93eda07ddcff54207d6d016ca1b0c2e
root <root@DESKTOP-S8OJ033.>
2024-01-09 16:56:05 +0200

    dine

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,4 +5,5 @@ This module check if its instance of inherited class or specified class
 
 
 def is_kind_of_class(obj, a_class):
+    """return True or false"""
     return isinstance(obj, a_class) or type(obj) is is_kind_of_class
