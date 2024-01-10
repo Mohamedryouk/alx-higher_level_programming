1a5b8a4f57a906ea73164f33d9276768276972ed
root <root@DESKTOP-S8OJ033.>
2024-01-10 15:43:59 +0200

    done

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains a function to add attribute
+"""
+
+
+def add_attribute(a_class, name, value):
+    if hasattr(a_class, "__dic__"):
+        setattr(a_class, name, value)
+    else:
+        raise TypeError("can't add new attribute") 
