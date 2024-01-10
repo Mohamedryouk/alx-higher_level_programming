b37a7d8dad7c824659e578b9f6878341f198505b
root <root@DESKTOP-S8OJ033.>
2024-01-10 15:47:41 +0200

    done

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,6 +5,7 @@ Contains a function to add attribute
 
 
 def add_attribute(a_class, name, value):
+    """Adds a new attribute to an obj if possible"""
     if hasattr(a_class, "__dic__"):
         setattr(a_class, name, value)
     else:
