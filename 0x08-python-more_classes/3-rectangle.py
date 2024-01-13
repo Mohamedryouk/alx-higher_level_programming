44ade4739c9f995efb9d712ff72cb7ba54ef5deb
root <root@DESKTOP-S8OJ033.>
2024-01-13 10:55:44 +0200

    done

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""
+Module 3-rectangle
+contains rectangle with attributes width and height
+"""
+
+
+class Rectangle:
+    """
+    Defines class rectangle with private attribute width and height
+    Args:
+        width (int): width
+        height (int): height
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+        area(self)
+        perimeter(self)
+    """
+
+    def __init__(self, width=0 , height=0):
+      """Initialize class"""
+      self.width = width
+      self.height = height
+
+    @property
+    def width(self):
+      """Getter for width"""
+      return self.__width
+
+    @width.setter
+    def width(self, value):
+      """settter for width"""
+      if not isinstance(value, int):
+        raise TypeError("width must be an integer")
+      if value < 0:
+        raise ValueError("width must be >= 0")
+      self.__width = value
+
+    @property
+    def height(self):
+      """Getter for height"""
+      return self.__height
+
+    @height.setter
+    def height(self, value):
+      """setter for height"""
+      if not isinstance(value, int):
+        raise TypeError("height must be an integer")
+      if value < 0:
+        raise ValueError("height must be >= 0")
+      self.__height = value
+
+    def area(self):
+        """area of rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+      """perimeter of rectangle"""
+      if self.width == 0 or self.height == 0:
+          return 0
+      return 2 * self.width * self.height
+
+    def __str__(self):
+        """str function returns in # format"""
+        if self.width == 0 or self.height == 0:
+            return ""
+        rectangle_str = ""
+        for _ in range(self.height):
+            rectangle_str += '#' * self.width + '\n'
+        return rectangle_str.rstrip('\n')
