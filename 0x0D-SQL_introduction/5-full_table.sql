9f83ae5197e7d4fd09b4d1556cffb307f6598206
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-03-14 15:39:08 +0300

    done

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- show full description of the tabel first_tabel.
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_tabel';
+SHOW CREATE TABLE `first_table`;
