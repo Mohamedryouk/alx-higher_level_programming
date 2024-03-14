cd9b51cbe755f3167c0e5ded5f1cd356ce6510ce
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-03-14 15:13:39 +0300

    done

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- create a table in the current database in MySQL server.
-CREATE TABLE IF NOT EXISTS 'first_table' (
-    'id' INT,
-    'name' VARCHAR(256),
-);
+CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256),);
