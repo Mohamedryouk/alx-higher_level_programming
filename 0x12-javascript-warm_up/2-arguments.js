61e4a29f1e091d0ef3de98829fbf08ecaf5a3932
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-08 16:27:59 +0300

    done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
+
+
 const arg = 'No argument';
 const Arg = 'Argument found';
-if (process.argv.length === 2) {
+if (process.argv.length <= 2) {
   console.log(arg);
-} else if (process.argv.length === 3) {
+} else {
+if (process.argv.length === 3) {
   console.log(Arg);
 } else {
   console.log('Argument found');
+}
 }
