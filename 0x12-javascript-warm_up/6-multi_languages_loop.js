c42acb3de2722506c3c315651bfafa6c5173765e
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-09 12:41:12 +0300

    done

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+ const first = 'C is fun';
+ const second = 'Python is cool';
+ const third = 'JavaScript is amazing';
+ let array = [first, second, third];
+for (let i = 0; i < array.length; i++)
+{
+    console.log(array[i]);
+}
