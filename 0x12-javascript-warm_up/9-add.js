d32831e7362780e88ea7098141f3958c827ed1be
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-09 15:23:34 +0300

    done

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function add(a, b) {
+    a = parseInt(process.argv[2]);
+    b = parseInt(process.argv[3]);
+
+    if (isNaN(a) || isNaN(b)) {
+        console.log('NaN');
+    } else {
+        console.log(a + b);
+    }
+}
+
+add();
