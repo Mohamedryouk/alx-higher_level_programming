5d146f23d6e1acae99a4af633770906dfa21c4d2
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-09 13:09:07 +0300

    done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const arg = 'C is fun';
 const num = process.argv[2];
+if (num === undefined) {
+    console.log('Missing number of occurences');
+}
 for (let i = 0; i < num; i++) {
 	console.log(arg);
 }
