385572987caf5879e22599964909f731b655b640
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-09 15:24:32 +0300

    done

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-function add(a, b) {
-    a = parseInt(process.argv[2]);
-    b = parseInt(process.argv[3]);
+function add (a, b) {
+  a = parseInt(process.argv[2]);
+  b = parseInt(process.argv[3]);
 
-    if (isNaN(a) || isNaN(b)) {
-        console.log('NaN');
-    } else {
-        console.log(a + b);
-    }
+  if (isNaN(a) || isNaN(b)) {
+    console.log('NaN');
+  } else {
+    console.log(a + b);
+  }
 }
 add();
