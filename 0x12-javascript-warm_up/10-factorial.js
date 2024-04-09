85460a74414a83214ba71e6b68082e8991d5ff0b
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-09 18:13:37 +0300

    done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-function factorial(x) {
-    if (x === 0 || x === 1) {
-        return 1;
-    } else if (isNaN(x)) {
-        return 1;
-    } else {
-        return x * factorial(x - 1);
-    }
+function factorial (x) {
+  if (x === 0 || x === 1) {
+    return 1;
+  } else if (isNaN(x)) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
 }
 const input = parseInt(process.argv[2]);
 
