bf3f774ac2c02e31acdd5baf37b1536ffc88ed80
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-09 12:36:47 +0300

    done

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 const firstArgument = parseInt(process.argv[2]);
-if (isNaN(firstArgument))
-{
-    console.log('Not a number');
+if (isNaN(firstArgument)) {
+  console.log('Not a number');
 } else {
-    console.log('My number: ' + firstArgument)
+  console.log('My number: ' + firstArgument);
 }
