4a4572f98aa718394b2044ef413b2733f6b0f807
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-09 18:12:27 +0300

    done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 function factorial(x) {
-    if (isNaN(x)) {
+    if (x === 0 || x === 1) {
+        return 1;
+    } else if (isNaN(x)) {
         return 1;
     } else {
         return x * factorial(x - 1);
