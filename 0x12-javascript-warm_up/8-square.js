5602d8e99499aeb51e720ecb615f11c765f57bb1
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-09 13:34:16 +0300

    done

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const firstArgument = process.argv[2];
+if (firstArgument === undefined) {
+  console.log('Missing size');
+}
+for (let i = 0; i < firstArgument; i++) {
+  let row = '';
+  for (let j = 0; j < firstArgument; j++) {
+    row += 'X';
+  }
+  console.log(row);
+}
