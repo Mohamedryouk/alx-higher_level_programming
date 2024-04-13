677e2a5e48756c3ec00f54ac3f79b19b622bf401
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-13 18:56:42 +0300

    done

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,3 @@
 -- Create database
-USE 'hbtn_0c_0'
-DROP DATABASE IF EXISTS hbtn_0c_0;
-ALTER TABLE 'first_table' CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
