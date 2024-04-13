aeab938f24793b1d2e6b5ab1bf0ffc951f9b6ca8
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-13 18:41:20 +0300

    done

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,15 +1,28 @@
--- Convert database to UTF8
+-- Create database 
+DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+USE hbtn_0c_0;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT, 
+    name VARCHAR(256),
+    score INT
+);
+
+-- Display first_table description
+SHOW CREATE TABLE first_table;
+
+-- Convert database and table to UTF8
 ALTER DATABASE hbtn_0c_0
     CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_unicode_ci;
 
--- Convert first_table to UTF8
 ALTER TABLE hbtn_0c_0.first_table
     CONVERT TO CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 
--- Modify name column to UTF8
 ALTER TABLE hbtn_0c_0.first_table
     MODIFY name VARCHAR(256)
     CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;i;
+    COLLATE utf8mb4_unicode_ci;
