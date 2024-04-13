9f14f70375730afa6fdef07bb6a1383f3fb4864c
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-13 18:38:47 +0300

    done

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,13 +1,15 @@
---script that converts hbtn_0c_0 database to UTF8
+-- Convert database to UTF8
 ALTER DATABASE hbtn_0c_0
     CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_unicode_ci;
 
+-- Convert first_table to UTF8
 ALTER TABLE hbtn_0c_0.first_table
     CONVERT TO CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE hbtn_0c_0.first_table 
+-- Modify name column to UTF8
+ALTER TABLE hbtn_0c_0.first_table
     MODIFY name VARCHAR(256)
     CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+    COLLATE utf8mb4_unicode_ci;i;
