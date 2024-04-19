93f8626b5e9ca2ab33c10f2e97321b18dd673985
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-19 09:39:13 +0300

    fixing tasks

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
@@ -8,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    """create engine"""
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
