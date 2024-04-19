62fc38e45f236e133355e7e27f46b7bc9a1feca1
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-19 08:45:02 +0300

    fix 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,16 +4,22 @@ import MySQLdb
 import sys
 
 fi = 34
+
+
 def list_states_with_n(username, password, database):
+    """listing states"""
     try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        db = MySQLdb.connect(host="localhost", port=3306,
+                             user=username, passwd=password,
+                             db=database)
         cursor = db.cursor()
 
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        cursor.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
         results = cursor.fetchall()
         for row in results:
             print(row)
-        
+
         db.close()
     except MySQLdb.Error as e:
         print(f"Error connecting to MySQL: {e}")
