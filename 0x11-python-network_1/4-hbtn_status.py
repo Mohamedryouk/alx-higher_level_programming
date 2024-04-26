fe3f0b374c2fd607bbbd5cba8f94dbcd3f27d8d2
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-26 22:27:23 +0300

    done

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#Fetching a URL using urllib package.
+import requests
+if __name__ == "__main__":
+    url = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: <class '{}'>".format(url.text.__class__))
+    print("\t- content: {}".format(url.text))
