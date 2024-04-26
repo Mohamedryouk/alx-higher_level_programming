7aab8c9d6684de1710c691ec0807ee151253af75
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-26 12:27:23 +0300

    done

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#Script that takes in a URL and sends
+#a request to the URL and displays the values
+
+import urllib.request
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit(1)
+#Accepting a URL
+url = sys.argv[1]
+#Try sending a request to the added URL
+try:
+    with urllib.request.urlopen(url) as response:
+#getting the request id and stor it in request_id and formating it using print.
+        request_id = response.headers.get("X-Request-Id")
+        print("{}".format(request_id))
+#exception handler for URL Errors.
+except Exception as e:
+    print("Error:", e)
