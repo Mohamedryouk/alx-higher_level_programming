ce34a42d8d17392451c1daf2f75413d119820150
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-26 12:09:20 +0300

    fetching url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,3 +9,7 @@ with urllib.request.urlopen(url) as response:
     body = response.read()
     body_str = body.decode('utf-8')
     content_type = type(body).__name__
+    print("Body response:")
+    print("\t- type: <class '{}'>".format(content_type))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body_str))
