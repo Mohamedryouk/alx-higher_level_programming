0f96800f54b0f6463a93162eb2452ee29bf7ca69
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-27 09:19:21 +0300

    done

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-Printing the HTTPError code
+Module for printing HTTPError code or connection error reason.
 """
-import urllib
+
 import sys
-from urllib.error import URLError, HTTPError
 import urllib.request
+from urllib.error import URLError, HTTPError
 
-
-if __name__ == "__main__":
+def main(url):
     """
     Main function to print the content of the URL or handle errors.
 
@@ -18,13 +17,22 @@ if __name__ == "__main__":
     Returns:
         None
     """
-    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode("UTF-8")
+            print(body)
+    except HTTPError as e:
+        print(f"Error code: {e.code}")
+    except URLError as er:
+        print(f"Connection error: {er.reason}")
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read().decode("UTF-8")
-        print(body)
-except HTTPError as e:
-    print(f"Error code: {e.code}")
-except URLError as er:
-    print(f"Connection error: {er.reason}")
+if __name__ == "__main__":
+    """
+    Main entry point of the script.
+    """
+    if len(sys.argv) != 2:
+        print("Usage: {} <URL>".format(sys.argv[0]))
+        sys.exit(1)
+
+    url = sys.argv[1]
+    main(url)
