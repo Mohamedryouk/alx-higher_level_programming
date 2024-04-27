e4e3db435bb4b197d1ce585bc024e3f5c8701393
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-27 10:37:22 +0300

    done

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,39 +8,11 @@ import urllib.request
 import urllib.parse
 
 
-def postEmail(url, email):
-    """
-        Sends a POST request to the specified URL with the given email.
-
-        Args:
-            url (str): The URL to send the POST request to.
-            email (str): The email to be sent in the POST request.
-
-        Returns:
-            None
-
-        Raises:
-            Exception: If an error occurs during the POST request.
-        """
-    url = sys.argv[1]
-    email = sys.argv[2]
-    print("Your email is:", email)
-    emailBytes = email.encode('utf-8')
-    try:
-        # Send a POST request to the url and email passed
-        with urllib.request.urlopen(url, data=emailBytes) as response:
-            # Read the response and print it to the user
-            body = response.read().decode('utf-8')
-            print("Response body:", body)
-    except Exception as e:
-        pass
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: {} <URL> <enmil>".format(sys.argv[0]))
-        sys.exit(1)
-
     url = sys.argv[1]
-    email = sys.argv[2]
-    postEmail(url, email)
+    emailData = {"email": sys.argv[2]}
+    pasring = urllib.parse.urlencode(emailData).encode("ascii")
+
+    request = urllib.request.Request(url, pasring)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
