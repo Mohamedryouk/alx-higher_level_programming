2388c9837f91968e446a56b7e3a350d8c34225f9
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-27 09:17:08 +0300

    done

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-# Fetching a URL using urllib package.
+"""
+Fetching a URL using the requests package.
+"""
+
 import requests
 
 
 if __name__ == "__main__":
     """
-    main: name
+    Main entry point of the script.
     """
     url = requests.get('https://alx-intranet.hbtn.io/status')
+
     print("Body response:")
     print("\t- type: <class '{}'>".format(url.text.__class__))
     print("\t- content: {}".format(url.text))
