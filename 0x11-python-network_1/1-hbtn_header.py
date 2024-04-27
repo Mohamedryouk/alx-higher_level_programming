99ce3c7cbda098200271e0d6c2b3dc41f38fbc92
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-27 10:21:22 +0300

    done

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3
 """
-    request the response ID
+Request the X-Request-Id from a given URL.
+
+This script takes a URL as a command-line argument and sends a request to it.
+It then extracts the X-Request-Id header from the response and prints it.
+
+Usage:
+    ./script_name.py <URL>
+
+Example:
+    ./script_name.py https://example.com
 """
+
 import urllib.request
 import sys
 
-if len(sys.argv) < 2:
-    sys.exit(1)
-# Accepting a URL
-url = sys.argv[1]
-# Try sending a request to the added URL
-try:
-    with urllib.request.urlopen(url) as response:
-        # getting the request id and stor
-        # it in request_id and formating it using print.
-        request_id = response.headers.get("X-Request-Id")
-        print("{}".format(request_id))
-except Exception as e:
-    # exception handler for URL Errors.
-    print("Error:", e)
+if __name__ == "__main__":
+    """
+    Main entry point of the script.
+    """
+    if len(sys.argv) < 2:
+        print("Usage: {} <URL>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Accepting a URL
+    url = sys.argv[1]
+
+    # Try sending a request to the provided URL
+    try:
+        with urllib.request.urlopen(url) as response:
+            # Getting the request id from the response headers
+            request_id = response.headers.get("X-Request-Id")
+            print("Request ID:", request_id)
+    except Exception as e:
+        # Exception handler for URL errors
+        print("Error:", e)
