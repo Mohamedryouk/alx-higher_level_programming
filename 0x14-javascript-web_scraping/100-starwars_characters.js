8412245d63129ee96342b36aad66af586b244726
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-06-05 13:16:53 +0300

    done

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,23 +1,44 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const request = require('request');
-const url = process.argv[2];
 
-request.get(url, { json: true }, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
+const movieId = process.argv[2];
+if (!movieId) {
+    console.error('Please provide a movie ID');
+    process.exit(1);
+}
 
-  const tasks = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!tasks[todo.userId]) {
-        tasks[todo.userId] = 1;
-      } else {
-        tasks[todo.userId] += 1;
-      }
+const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(movieUrl, (error, response, body) => {
+    if (error) {
+        console.error('Error fetching movie details:', error);
+        return;
     }
-  });
-  console.log(tasks);
+
+    if (response.statusCode !== 200) {
+        console.error('Failed to fetch movie details');
+        return;
+    }
+
+    const movieData = JSON.parse(body);
+    const characterUrls = movieData.characters;
+
+    characterUrls.forEach(url => {
+        request(url, (charError, charResponse, charBody) => {
+            if (charError) {
+                console.error('Error fetching character details:', charError);
+                return;
+            }
+
+            if (charResponse.statusCode !== 200) {
+                console.error('Failed to fetch character details');
+                return;
+            }
+
+            const characterData = JSON.parse(charBody);
+            console.log(characterData.name);
+        });
+    });
 });
+
