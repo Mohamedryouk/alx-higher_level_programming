d848d6cf26e40c3160c7896922df87597ffd4c94
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-06-05 13:35:16 +0300

    done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,55 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const movieId = process.argv[2];
-if (!movieId) {
-    console.error('Please provide a movie ID');
-    process.exit(1);
-}
-
-const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(movieUrl, (error, response, body) => {
-    if (error) {
-        console.error('Error fetching movie details:', error);
-        return;
-    }
-
-    if (response.statusCode !== 200) {
-        console.error('Failed to fetch movie details');
-        return;
-    }
-
-    const movieData = JSON.parse(body);
-    const characterUrls = movieData.characters;
-
-    function fetchCharacterName(url) {
-        return new Promise((resolve, reject) => {
-            request(url, (charError, charResponse, charBody) => {
-                if (charError) {
-                    reject(charError);
-                    return;
-                }
-
-                if (charResponse.statusCode !== 200) {
-                    reject(new Error('Failed to fetch character details'));
-                    return;
-                }
-
-                const characterData = JSON.parse(charBody);
-                resolve(characterData.name);
-            });
-        });
-    }
-
-    const characterPromises = characterUrls.map(url => fetchCharacterName(url));
-
-    Promise.all(characterPromises)
-        .then(names => {
-            names.forEach(name => console.log(name));
-        })
-        .catch(err => {
-            console.error('Error fetching character names:', err);
-        });
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
